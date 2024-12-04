c8527685ecfe45b0933bb9847fff1c3a27603b48
nilszeilon <nils.barr.zeilon@gmail.com>
2024-12-04 08:57:16 +0100

    add vimwiki diary template

--- nvim/lua/plugins/vimwiki.lua
+++ nvim/lua/plugins/vimwiki.lua
@@ -15,17 +15,38 @@
 return {
   "vimwiki/vimwiki",
   init = function()
+    -- Define the diary template
+    local diary_template = [[# %title%
+
+wake up time:  
+
+]]
+
     -- Use Markdown syntax
     vim.g.vimwiki_list = {
       {
         path = "~/vimwiki",
         syntax = "markdown",
         ext = ".md",
+        diary_rel_path = "diary/",
       },
     }
     -- Make sure vimwiki doesn't own all .md files
     vim.g.vimwiki_global_ext = 0
     -- Register markdown as the parser for vimwiki files (for treesitter support)
     vim.treesitter.language.register("markdown", "vimwiki")
+
+    -- Auto-apply template to new diary files
+    vim.api.nvim_create_autocmd("BufNewFile", {
+      pattern = vim.fn.expand("*/vimwiki/diary/*.md"),
+      callback = function()
+        local filename = vim.fn.expand("%:t:r") -- Get filename without extension
+        local content = diary_template:gsub("%%title%%", filename)
+        local lines = vim.split(content, "\n")
+        vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+        -- Position cursor after the Tasks header
+        vim.cmd("normal! 4G$")
+      end,
+    })
   end,
 }
